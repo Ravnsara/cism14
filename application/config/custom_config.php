3c2b757144dbbf6445c9753aaed01cec9e3753f3
Christina Rome <aegis_42@yahoo.com>
2014-07-17 18:52:16 -0700

    Adding new bootswatch theme

---
+++ application/config/custom_config.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+//custom_config.oho - a place to save global setttings
+
+$config['style'] = 'flatly.css';
+$config['banner'] = 'Default Banner';
+$config['title'] = 'Default Title';
+$config['copyright'] = 'Default Copyright';
+$config['theme'] = '';
+
